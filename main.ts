2546399e66dec2381ccfedfe0123835e55daffc6
kuhnino <77860530+kuhnino@users.noreply.github.com>
2021-11-09 05:57:59 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,18 @@
+let Temp = 0
+serial.redirectToUSB()
+let Timer = 15
+Timer = Timer * 60
+Timer = Timer - 1
+Timer = Timer * 1000
+Timer = Timer * 1000
+Timer = 10000
+basic.forever(function () {
+    Temp = DS18B20.TemperatureNumber(DS18B20.pin.pin16)
+    Temp = Temp * 10
+    Temp = Math.round(Temp)
+    Temp = Temp / 10
+    basic.showString("" + Temp)
+    serial.writeString("" + Temp)
+    control.waitMicros(Timer)
+    Temp = 0
+})
